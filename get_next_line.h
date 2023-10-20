d2154be0c7bc5027ab75d0cdfd04e90addbbea90
Alexander Weisshaar <aweissha@2-C-6.42heilbronn.de>
2023-10-20 16:41:13 +0200

    created files

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aweissha <aweissha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 16:35:01 by aweissha          #+#    #+#             */
+/*   Updated: 2023/10/20 16:37:59 by aweissha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "libft.h"
+
+#endif
